f5b39bb10ba6e39adcc51949308b5969159d9811
Chris Rocha <chris@chrisrocha.com>
2022-01-05 11:33:02 -0500

    added twigs, updated slider and site.js, preprocesses

--- themes/custom/nightcat/js/site.js
+++ themes/custom/nightcat/js/site.js
@@ -1,22 +1,24 @@
-jQuery(document).ready(function() {
-    jQuery('<li class="br"></li>').insertAfter('.menu-315');
+(function ($, Drupal) {
+  $(document).ready(function() {
+    $('<li class="br"></li>').insertAfter('.menu-315');
 
-    jQuery('.region-content .node .content').bxSlider({
+    $('#content .node--type-portfolio .node__content').bxSlider({
         auto: true,
         speed: 300,
         nextText: '&raquo;',
         prevText: '&laquo;',
         pager: false,
         onSliderLoad: function($currentIndex) {
-            var alt = jQuery('.region-content .node .content .slide').eq(1).find('img').attr('alt');
-            jQuery('#caption').html('<p>' + urlToLink(alt) + '</p>');
+            var alt = $('#content .node--type-portfolio .node__content .slide').eq(1).find('img').attr('alt');
+            $('#caption').html('<p>' + urlToLink(alt) + '</p>');
         },
         onSlideAfter: function($slideElement, oldIndex, newIndex) {
             var alt = $slideElement.find('img').attr('alt');
-            jQuery('#caption').html('<p>' + urlToLink(alt) + '</p>');
+            $('#caption').html('<p>' + urlToLink(alt) + '</p>');
         }
     });
-});
+  }); // document ready
+})(jQuery, Drupal);
 
 function urlToLink(text) {
     return text.replace(/\[url=([^\]]+)\]\s*(.*?)\s*\[\/url\]/ig, '<a href="$1" target="_blank">$2</a>');
